68c24e0ac7e48de83a116f044e58e9a39188f863
YashLonkar <yashlonkar99@gmail.com>
2024-06-03 20:25:36 +0530

    Fixed Screen Freeze Issue

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { v4 as uuid } from 'uuid'; // Correct import for uuid
-import './App.css';
-import api from "../api/contacts";
+import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
+import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
-import ContactDetail from "./ContactDetail";
 
 function App() {
   const LOCAL_STORAGE_KEY = "contacts";
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []
+  );
 
-  // Retrieve contacts from the API
-  const retrieveContacts = async () => {
-    const response = await api.get("/contacts");
-    return response.data;
-  }
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
+  };
 
-  const addContactHandler = async (contact) => {
-    const request = {
-      id: uuid(),
-      ...contact,
-    };
-    const response = await api.post("/contacts", request); // await added here
-    setContacts([...contacts, response.data]);
-  }
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
 
-  const removeContactHandler = async (id) => {
-    await api.delete(`/contacts/${id}`);
-    const newContactList = contacts.filter((contact) => contact.id !== id);
     setContacts(newContactList);
   };
 
-  useEffect(() => {
-    const retrieveContactsFromLocalStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (retrieveContactsFromLocalStorage) {
-      setContacts(retrieveContactsFromLocalStorage);
-    }
-
-    const getAllContacts = async () => {
-      const allContacts = await retrieveContacts();
-      if (allContacts) setContacts(allContacts);
-    };  
-
-    getAllContacts();
-  }, []);
+  // useEffect(() => {
+  //   const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  //   if (retriveContacts) setContacts(retriveContacts);
+  // }, []);
 
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
@@ -53,14 +35,9 @@ function App() {
 
   return (
     <div className="ui container">
-      <Router>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<ContactList contacts={contacts} getContactId={removeContactHandler} />} />
-          <Route exact path="/add" element={<AddContact addContactHandler={addContactHandler} />} />
-          <Route exact path="/contact/:id" element={<ContactDetail contacts={contacts} />} />
-        </Routes>
-      </Router>
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,37 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import ContactCard from "./ContactCard";
 
-const ContactList= (props) => {
-    console.log(props);
+const ContactList = (props) => {
+  console.log(props);
 
-    const deleteContactHandler = (id) => {
-        props.getContactId(id);
-    }
-
-
-    
-
-    const renderContactList = props.contacts.map((contact) => {
-        return (
-            <ContactCard contact= {contact} clickHandler  ={deleteContactHandler} key={contact.id}></ContactCard>
-        );
-    })
+  const deleteConactHandler = (id) => {
+    props.getContactId(id);
+  };
+  const renderContactList = props.contacts.map((contact) => {
     return (
-        <div class="main">
-           <div style={{display:"flex",justifyContent:"space-between", paddingTop:"60px"}}>
-           <h2 className="ui header">
-            ContactList
-            </h2>
-            <Link to="/add">
-            <button className="ui button blue right">Add Contact</button>
-            </Link>
-            
-           </div>
-            <div className="ui celled list">{renderContactList}</div>
-        </div>
-        
-    )
-}
+      <ContactCard
+        contact={contact}
+        clickHander={deleteConactHandler}
+        key={contact.id}
+      />
+    );
+  });
+  return <div className="ui celled list">{renderContactList}</div>;
+};
 
 export default ContactList;
