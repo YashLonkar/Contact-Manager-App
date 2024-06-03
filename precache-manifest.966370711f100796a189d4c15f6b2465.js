0ec66a82af654bab16a837b5cdb6313457b866f5
YashLonkar <yashlonkar99@gmail.com>
2024-06-03 20:32:20 +0530

    Updates

---
+++ precache-manifest.966370711f100796a189d4c15f6b2465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d67bf0ddd1bf8a397898b3b7e39dc2",
+    "url": "/Contact-Manager-App/index.html"
+  },
+  {
+    "revision": "54d1430db3f11042bc8a",
+    "url": "/Contact-Manager-App/static/css/main.0de8f683.chunk.css"
+  },
+  {
+    "revision": "7eca399c581e1ae23e10",
+    "url": "/Contact-Manager-App/static/js/2.11a76167.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Contact-Manager-App/static/js/2.11a76167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d1430db3f11042bc8a",
+    "url": "/Contact-Manager-App/static/js/main.59fd15b4.chunk.js"
+  },
+  {
+    "revision": "8daa8f88efa87efe475d",
+    "url": "/Contact-Manager-App/static/js/runtime-main.5f8ca704.js"
+  }
+]);
